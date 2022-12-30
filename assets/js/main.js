f7c7ad00782ad1c5c968fb1a4a2d2dc61c5afe9d
Ali <aliarthur35@gmail.com>
2022-12-29 23:56:19 -0500

    added skills sectio

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,15 +22,30 @@ if(navClose){
 const navLink = document.querySelectorAll('.nav_link')
 
 function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
+  const navMenu = document.getElementById('nav-menu')
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills_content'),
+  skillsHeader = document.querySelectorAll('.skills_header')
 
+function toggleSkills(){
+  let itemClass = this.parentNode.className
 
+  for(i = 0; i < skillsContent.length; i++){
+    skillsContent[i].className = 'skills_content skills_close'
+  }
+  if(itemClass === 'skillsContent skills_close'){
+    this.parentNode.className = 'skills_content skills_open'
+  }
+}
+
+skillsHeader.forEach((el) =>{
+  el.addEventListener('click', toggleSkills)
+})
 /*==================== QUALIFICATION TABS ====================*/
 
 
