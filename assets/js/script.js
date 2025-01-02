3d9e0c8091ff3927c59900b20b3fd2cd5d4258d1
Ali Arthur <121364807+Savant35@users.noreply.github.com>
2025-01-02 17:35:25 -0500

    added code to highlight active location in nav menu and added code for email.js to send emails using the form currently commented out

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,14 +53,59 @@ const debounce = (func, delay) => {
 
 window.addEventListener('scroll', debounce(shadowHeader, 100));
   /*=============== EMAIL JS ===============*/
+/*
+ const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message');
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  // Replace placeholders with your actual service ID, template ID, and public key
+  emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', '#contact-form', 'YOUR_PUBLIC_KEY')
+    .then((response) => {
+
+      // Show success message
+      contactMessage.textContent = 'Message sent successfully ✅';
+
+      // Remove message after 10 seconds
+      setTimeout(() => {
+        contactMessage.textContent = '';
+      }, 10000);
+
+      // Clear input fields
+      contactForm.reset();
+    }, (error) => {
+      // Show error message
+      contactMessage.textContent = 'Message not sent (service error) ❌';
+    });
+};
+
+// Attach the event listener
+contactForm.addEventListener('submit', sendEmail);
+*/
 
 
-  /*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
 
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 58;
+    const sectionId = current.getAttribute('id');
+    const linkSelector = `.nav__menu a[href*="${sectionId}"]`;
+    const sectionLink = document.querySelector(linkSelector);
 
-  /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionLink?.classList.add('active-link');
+    } else {
+      sectionLink?.classList.remove('active-link');
+    }
+  });
+};
 
-
+window.addEventListener('scroll', scrollActive);
   /*=============== DARK LIGHT THEME ===============*/ 
 
 
